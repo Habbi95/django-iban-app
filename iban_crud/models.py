f8e56e13f2408265c745d47cd6774bb4b1219372
Javier Vargas <javiervargasvico@hotmail.com>
2021-12-30 23:23:17 +0100

    Added IBAN Sepa countries to model + migrations

--- iban_crud/models.py
+++ iban_crud/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from localflavor.generic.models import IBANField
+from localflavor.generic.countries.sepa import IBAN_SEPA_COUNTRIES
 
 # Create your models here.
 class IBANUser(models.Model):
@@ -8,7 +9,7 @@ class IBANUser(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    iban = IBANField()
+    iban = IBANField(include_countries=IBAN_SEPA_COUNTRIES)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} IBAN: {str(self.iban)}'
