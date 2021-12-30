f9b285517c8bd370e6375ed5a2a571263ec984b6
Javier Vargas <javiervargasvico@hotmail.com>
2021-12-30 23:22:41 +0100

    Updated template DIR (see previous commit)

--- iban_app/settings.py
+++ iban_app/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'iban_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'allauth_templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
