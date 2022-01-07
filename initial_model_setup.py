0921e33fa6de701bd7628cbcc186d7adc677dcf4
Javier Vargas Vico <javiervargasvico@hotmail.com>
2022-01-07 15:42:26 +0100

    Update initial_model_setup.py

--- initial_model_setup.py
+++ initial_model_setup.py
@@ -8,10 +8,10 @@ try:
 except User.DoesNotExist:
     User.objects.create_superuser('admin', 'admin@example.com', 'admin')
 
-# OAtuh django-allauth setup. It uses personal OAuth set up by me, please when all it's tested notify me to delete the app in my account
+# OAtuh django-allauth setup.
 site, site_created = Site.objects.get_or_create(domain='127.0.0.1:8000', name='127.0.0.1:8000')
 social_app, social_app_created = SocialApp.objects.get_or_create(provider='google', name='oauth-iban-app',
-                                      client_id='883826469396-l398ib8fp4v1ng49rbcmu7v82cnnppdj.apps.googleusercontent.com', 
-                                      secret='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')
+                                      client_id='<client_id>', 
+                                      secret='<secret>')
 if social_app_created:
     social_app.sites.set([site])
